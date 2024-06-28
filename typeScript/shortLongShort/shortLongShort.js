968313cbd8fe679c29fc6d1ecf1a01aaf127d6c4
fravila08 <fr4v1l4@gmail.com>
2024-06-28 07:26:59 -0700

    shortLongShort with TS compiled onto js

--- typeScript/shortLongShort/shortLongShort.js
+++ typeScript/shortLongShort/shortLongShort.js
@@ -1,9 +1,8 @@
 // consider one strings length will ALWAYS be bigger or smaller than the other string.
 // assume you will always receive strings
-const shortLongShort = (a: string, b: string): string => {
-  return a.length < b.length ? a + b + a : b + a + b;
+var shortLongShort = function (a, b) {
+    return a.length < b.length ? a + b + a : b + a + b;
 };
-
 console.log(shortLongShort('45', '1') === '1451');
 console.log(shortLongShort('13', '200') === '1320013');
 console.log(shortLongShort('Soon', 'Me') === 'MeSoonMe');
